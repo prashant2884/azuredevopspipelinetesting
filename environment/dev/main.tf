750f62ee711f52d3d45fe59fb400942c7978b278
prashant2884 <prashant.gupta08@gmail.com>
2026-08-25 20:32:08 +0530

    add rg

---
+++ environment/dev/main.tf
@@ -0,0 +1,5 @@
+module "resource_group" {
+  source = "../../module/azurerm_resource_group"
+  pr     = var.pr1
+}
+
---
+++ module/azurerm_resource_group/terraform.tfvars
@@ -0,0 +1,6 @@
+pr = {
+  rg1 = {
+    name     = "gupta"
+    location = "Central india"
+  }
+}
